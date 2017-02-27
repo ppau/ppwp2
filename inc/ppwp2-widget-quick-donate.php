b04ce8ba007547d69e4bef047f71aae7108ffc00
Thomas Randle <thomas@tasgn.com>
2017-02-27 19:21:50 +1100

    Make quick donate popup amount editable

--- inc/ppwp2-widget-quick-donate.php
+++ inc/ppwp2-widget-quick-donate.php
@@ -1,10 +1,13 @@
 <?php
-// Creating the widget 
-class ppwp2_widget_quick_donate extends WP_Widget {
-	function __construct() {
+
+// Creating the widget
+class ppwp2_widget_quick_donate extends WP_Widget
+{
+	function __construct()
+	{
 		parent::__construct(
 
-			// Base ID of your widget
+		// Base ID of your widget
 			'ppwp2_widget_quick_donate',
 
 			// Widget name will appear in UI
@@ -12,92 +15,99 @@ class ppwp2_widget_quick_donate extends WP_Widget {
 
 			// Widget description
 			array(
-				'description' 	=> __( 'Card for quick donate', 'ppwp2' ),
+				'description' => __('Card for quick donate', 'ppwp2'),
 			)
 		);
 	}
 
-	public function uri_for_amount($pin_uri, $amount){
-	    return $pin_uri . "?amount=%24" . $amount . "&amp;description=One-off+donation&amp;amount_editable=false&amp;success_url=https%3A%2F%2Fpirateparty.org.au%2Fdonate-thank-you%2F";
-    }
+	public function uri_for_amount($pin_uri, $amount)
+	{
+		return $pin_uri . "?amount=%24" . $amount . "&amp;description=One-off+donation&amp;amount_editable=true&amp;success_url=https%3A%2F%2Fpirateparty.org.au%2Fdonate-thank-you%2F";
+	}
 
 	// Creating widget front-end
 	// This is where the action happens
-	public function widget( $args, $instance ) {
-		extract( $args );
+	public function widget($args, $instance)
+	{
+		extract($args);
 
-        $pin_uri = apply_filters('widget_pin_uri', $instance['pin_uri']);
+		$pin_uri = apply_filters('widget_pin_uri', $instance['pin_uri']);
 
 		// before and after widget arguments are defined by themes
 		echo $before_widget;
 
-        ?>
+		?>
         <div class="pir-card pir-card__quick-donate mdl-card mdl-shadow--2dp">
             <div class="mdl-card__title">
                 <h2 class="mdl-card__title-text">Quick donate</h2>
             </div>
             <div class="mdl-card__supporting-text">
-                <p>We run an efficient ship, but political parties are not cheap to run. Every dollar you donate goes a long way.</p>
+                <p>We run an efficient ship, but political parties are not cheap to run. Every dollar you donate goes a
+                    long way.</p>
                 <div class="mdl-grid">
                     <div class="mdl-cell mdl-cell--4-col">
                         <a href="<?php echo($this->uri_for_amount($pin_uri, "5.00")); ?>"
-                                class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          $5
+                           class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            $5
                         </a>
                     </div>
                     <div class="mdl-cell mdl-cell--4-col">
                         <a href="<?php echo($this->uri_for_amount($pin_uri, "10.00")); ?>"
-                                class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          $10
+                           class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            $10
                         </a>
                     </div>
                     <div class="mdl-cell mdl-cell--4-col">
-                        <a  href="<?php echo($this->uri_for_amount($pin_uri, "25.00")); ?>"
-                                class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          $25
+                        <a href="<?php echo($this->uri_for_amount($pin_uri, "25.00")); ?>"
+                           class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            $25
                         </a>
                     </div>
                 </div>
                 <div class="mdl-grid">
                     <div class="mdl-cell mdl-cell--4-col">
                         <a href="<?php echo($this->uri_for_amount($pin_uri, "50.00")); ?>"
-                                class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          $50
+                           class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            $50
                         </a>
                     </div>
                     <div class="mdl-cell mdl-cell--4-col">
                         <a href="<?php echo($this->uri_for_amount($pin_uri, "100.00")); ?>"
-                                class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          $100
+                           class="pin-payment-button pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            $100
                         </a>
                     </div>
                     <div class="mdl-cell mdl-cell--4-col">
-                        <a href="<?php echo(get_page_by_slug("donate")); ?>" class="pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                          Other
+                        <a href="<?php echo(get_page_by_slug("donate")); ?>"
+                           class="pir-button pir-button-block mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            Other
                         </a>
                     </div>
                 </div>
             </div>
         </div>
-        <?php
+		<?php
 
 		echo $after_widget;
 	}
 
 	// Widget Backend
-	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array(
-			'pin_uri' 		=> ''
-		) );
+	public function form($instance)
+	{
+		$instance = wp_parse_args((array)$instance, array(
+			'pin_uri' => ''
+		));
 
 		$pin_uri = esc_attr($instance['pin_uri']);
 
 		// Widget admin form
 		?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'pin_uri' ); ?>"><?php _e( 'Pin.js URI', 'ppwp2' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'pin_uri' ); ?>" name="<?php echo $this->get_field_name( 'pin_uri' ); ?>" type="text" value="<?php echo esc_attr( $pin_uri ); ?>" />
-		</p>
+        <p>
+            <label for="<?php echo $this->get_field_id('pin_uri'); ?>"><?php _e('Pin.js URI', 'ppwp2'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('pin_uri'); ?>"
+                   name="<?php echo $this->get_field_name('pin_uri'); ?>" type="text"
+                   value="<?php echo esc_attr($pin_uri); ?>"/>
+        </p>
 
 	<?php }
 
@@ -111,7 +121,8 @@ class ppwp2_widget_quick_donate extends WP_Widget {
 } // Class ppwp2_widget_quick_donate ends here
 
 // Register and load the widget
-function ppwp2_widget_quick_donate_loader() {
+function ppwp2_widget_quick_donate_loader()
+{
 	register_widget('ppwp2_widget_quick_donate');
 }
 

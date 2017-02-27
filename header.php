809793deee6a0345ffcdde42dcffb75bde10a0b7
Thomas Randle <thomas@tasgn.com>
2017-02-27 15:07:00 +1100

    Fixes favicons

--- header.php
+++ header.php
@@ -14,9 +14,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="shortcut icon" type="image/x-icon" href="<?php echo(get_template_directory_uri() . '/img/favicon.ico'); ?>" />
+
+	<link rel="icon" type="image/png" href="<?php echo(get_stylesheet_directory_uri() . '/img/favicon/favicon-16x16.png'); ?>" sizes="16x16">
+	<link rel="icon" type="image/png" href="<?php echo(get_stylesheet_directory_uri() . '/img/favicon/favicon-32x32.png'); ?>" sizes="32x32">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo(get_stylesheet_directory_uri() . '/img/favicon/apple-touch-icon.png'); ?>">
+	<link rel="manifest" href="<?php echo(get_stylesheet_directory_uri() . '/img/favicon/manifest.json'); ?>">
+	<link rel="mask-icon" href="<?php echo(get_stylesheet_directory_uri() . '/img/favicon/safari-pinned-tab.svg'); ?>" color="#5bbad5">
+	<meta name="theme-color" content="#0073ae">
 
 	<?php wp_head(); ?>
+
 
 </head>
 

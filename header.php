057d2acca9a689ade34cd43168d3eef8941cc999
Thomas Randle <thomas@tasgn.com>
2017-06-15 00:27:15 +1000

    seo nonsense

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
+<head <?php do_action( 'add_head_attributes' ); ?>>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">

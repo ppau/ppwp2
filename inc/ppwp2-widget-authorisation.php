fad821998f5cd3e9eac5eff9073e409109e76049
Thomas Randle <thomas@tasgn.com>
2017-08-27 12:23:04 +1000

    Add space for more details in the footer auth tag.

--- inc/ppwp2-widget-authorisation.php
+++ inc/ppwp2-widget-authorisation.php
@@ -24,6 +24,7 @@ class ppwp2_widget_authorisation extends WP_Widget {
 
 		$person_and_position = apply_filters( 'widget_person_and_position', $instance['person_and_position'] );
 		$address = apply_filters('widget_address', $instance['address']);
+		$extra_details = apply_filters('widget_extra_details', $instance['extra_details']);
 
 		// before and after widget arguments are defined by themes
 		echo $before_widget;
@@ -38,6 +39,10 @@ class ppwp2_widget_authorisation extends WP_Widget {
 			echo '<br>' . $address;
 		}
 
+		if (!empty($extra_details)) {
+			echo '<br>' . str_replace("\n", "<br>", htmlspecialchars($extra_details));
+		}
+
 		echo $after_widget;
 	}
 
@@ -45,11 +50,13 @@ class ppwp2_widget_authorisation extends WP_Widget {
 	public function form( $instance ) {
 		$instance = wp_parse_args( (array) $instance, array(
 			'person_and_position' => '',
-			'address' 		=> ''
+			'address' 		=> '',
+			'extra_details' => ''
 		) );
 
 		$person_and_position = esc_attr($instance['person_and_position']);
 		$address = esc_attr($instance['address']);
+		$extra_details = $instance['extra_details'];
 
 		// Widget admin form
 		?>
@@ -63,6 +70,11 @@ class ppwp2_widget_authorisation extends WP_Widget {
 			<input class="widefat" id="<?php echo $this->get_field_id( 'address' ); ?>" name="<?php echo $this->get_field_name( 'address' ); ?>" type="text" value="<?php echo esc_attr( $address ); ?>" />
 		</p>
 
+		<p>
+            <label for="<?php echo $this->get_field_id('extra_details'); ?>"><?php _e('Extra details', 'ppwp2'); ?>:</label>
+            <textarea class="widefat" rows="6" cols="20" id="<?php echo $this->get_field_id('extra_details'); ?>" name="<?php echo $this->get_field_name('extra_details'); ?>"><?php echo $extra_details; ?></textarea>
+        </p>
+
 	<?php }
 
 	// Updating widget replacing old instances with new
@@ -71,6 +83,7 @@ class ppwp2_widget_authorisation extends WP_Widget {
 		$instance = array();
 		$instance['person_and_position'] = !empty($new_instance['person_and_position']) ? strip_tags($new_instance['person_and_position']) : '';
 		$instance['address'] = !empty($new_instance['address']) ? strip_tags($new_instance['address']) : '';
+		$instance['extra_details'] = !empty($new_instance['extra_details']) ? strip_tags($new_instance['extra_details']) : '';
 		return $instance;
 	}
 } // Class ppwp2_widget_authorisation ends here
